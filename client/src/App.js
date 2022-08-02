ed6ab471a95311bbc832acb1e53c8f0fa2e66794
Kyle Jones <kyle@kylejones.dev>
2022-08-02 11:25:02 -0700

    Add development mode banner Adjust gitignore

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Link, Outlet } from "react-router-dom";
 function App () {
     return (
         <div className="menu">
+            {process.env.NODE_ENV === 'development' && <p>Running in {process.env.NODE_ENV} mode.</p>}
             <nav>
                 <Link to="/">Home</Link>
                 <Link to="/new-transaction">New Transaction</Link>
